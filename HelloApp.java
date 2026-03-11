435cdc387847e6ea558f4c99d13be15ddb6fffef
PRADIPTA <pradiptad2006@gmail.com>
2026-03-11 09:33:40 +0530

    Uc3 completed

--- HelloApp.java
+++ HelloApp.java
@@ -2,15 +2,14 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
+        // Default name
+        String name = "World";
+
+        // Check if a name is provided as command-line argument
         if (args.length > 0) {
-            // Get the name from command-line argument
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } 
-        else {
-            // Default message if no argument is given
-            System.out.println("Hello, World!");
+            name = args[0];   // Use the provided name
         }
 
+        System.out.println("Hello, " + name + "!");
     }
 }
